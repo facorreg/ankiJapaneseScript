061cd6f374280d80aacc39c4e063bd79c5b7a977
root <root@DESKTOP-51MGIR3.localdomain>
2020-09-06 00:10:51 +0200

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+const globalNames = [
+  /* env */
+  'KANJI_API_URL',
+  'JISHO_URL',
+  'JISHO_IMG_URL',
+  'SORRY_GIF',
+  'videoFormats',
+  'audioFormats',
+  /* utils */
+  'isEmpty',
+  'isArray',
+  'get',
+  'filterEmpty',
+  'uniq',
+  'insertElemAtIndex',
+  'getCurrentWord',
+  /* utils kanji handling */
+  'getKanji',
+  'hasKanji',
+  'stringWithFurigana',
+  /* utils fetch */
+  'myFetch',
+  /* utils formats */
+  'findValidFormat',
+  'objectListKeysToCamelCase',
+  'objectPropEnforceArray',
+  /* utils generator */
+  'elemGenerator',
+  'createCardChildren',
+  'createModalChildren',
+  /* kanji page build */
+  'buildKanjiPage',
+  'buildKanjiData',
+  'overviewData',
+  'sideBarData',
+  'strokesImagesData',
+  'examplesData',
+  /* word page build */
+  'defElemsData',
+  'otherFormsData',
+  'kanjiListData',
+  /* Any page build */
+  'setFinalDisplay',
+];
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    // 'no-use-before-define': ['off', { variables: false }],
+    'no-use-before-define': ['off', { variables: true }],
+  },
+  globals: {
+    ...globalNames
+      .reduce((acc, name) => ({ ...acc, [name]: 'readonly' }), {}),
+  },
+};
